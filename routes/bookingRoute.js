3b3fce51d4442b70e7f0f6b069fef3798bb41d69
unknown <raoyasirmajeed140@gmail.com>
2025-07-03 19:51:28 +0500

    changes

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const {authanticateToken,authorizeRole} = require("../middlewares/authMiddleware");
 const { createBooking, viewAppointments, cancelAppointment } = require('../controllers/bookingController');
 
-//....................... User Crate Appoinment ...............//
+// User Routes
 router.post("/create-booking/:id",authanticateToken,authorizeRole('user'),createBooking);
 
-//..................... View Appointments ...........//
+
 router.get("/view-appointments",authanticateToken,authorizeRole('user'),viewAppointments);
 
-//....................Cancelled Appointment ..........//
+
 router.post("/cancel-appointment/:id",authanticateToken,authorizeRole('user'),cancelAppointment)
 
 
--- routes/providerDeashboardRoute.js
+++ routes/providerDeashboardRoute.js
@@ -4,16 +4,16 @@ const router = express.Router();
 const {authanticateToken,authorizeRole} = require('../middlewares/authMiddleware')
 
 
-//............. Update/Create Provider Profile............//
+// Provider Routes 
 router.post('/add-profile',authanticateToken,authorizeRole('provider'),createProfile);
 
-//................ Get Profile ...........................//
+
 router.get("/get-profile",authanticateToken,authorizeRole('provider'),getProfile);
 
-//.................. Get Appointments ............//
+
 router.get('/appointments',authanticateToken,authorizeRole('provider'),getProviderAppointments);
 
-//................. Change Appointment Status ...............//
+
 router.patch("/change-status/:id",authanticateToken,authorizeRole('provider'),changeStatus);
 
 
